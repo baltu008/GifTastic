bc08eb10cfcdaa1b3cc1cf896f7dd72a8ca4fb20
Eric Baltutis <baltu008@umn.edu>
2018-04-19 22:43:33 -0500

    fixed bugs

--- assets/script.js
+++ assets/script.js
@@ -13,25 +13,29 @@ $(document).ready(function () {
 
     $("#animal-form").submit(function (e) {
         e.preventDefault();
-        var newAnimal = $('#animal-input').val();
-        $('#buttonView').append(`
-        <button class='animal-button'>
-        ${newAnimal}
-        </button>
-        `)
+        var newAnimal = $('#animal-input').val().trim();
+        if (newAnimal) {
+            $('#buttonView').append(`
+            <button class='animal-button'>
+            ${newAnimal}
+            </button>
+            `)
+        }
     });
 
 
     $(document).on("click", ".animal-button", function () {
         var buttonValue = ($(this).text().trim());
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?" + "q=" + buttonValue + "&apikey=7ZMRSxlX7lPZWvtLR7iPdhXmqURWKf7J" + "&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?" + "q=" + buttonValue + "&apikey=7ZMRSxlX7lPZWvtLR7iPdhXmqURWKf7J" + "&limit=15";
+
 
         $.ajax({
             url: queryURL,
             method: 'GET',
         }).then(function (response) {
             console.log(response);
+            $('#gifs-appear-here').empty()
             for (i = 0; i < response.data.length; i++) {
                 $('#gifs-appear-here').append(`
             <img src=${response.data[i].images.original.url} />
