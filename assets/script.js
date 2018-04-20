923d743109767493fd3c28cee9e4583098d775a0
Eric Baltutis <baltu008@umn.edu>
2018-04-20 00:25:36 -0500

    data-still gifs

--- assets/script.js
+++ assets/script.js
@@ -42,7 +42,20 @@ $(document).ready(function () {
             `)
             }
         })
+        // $(".animal-button").on("click", function() {
+        //     var state = $(this).attr("data-state");
+    
+        //     if (state === "still") {
+        //       $(this).attr("src", $(this).attr("data-animate"));
+        //       $(this).attr("data-state", "animate");
+        //     } else {
+        //       $(this).attr("src", $(this).attr("data-still"));
+        //       $(this).attr("data-state", "still");
+        //     }
+        //   });
     })
+
+
 
     renderButton();
 
